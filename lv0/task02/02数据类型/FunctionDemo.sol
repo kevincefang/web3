8d82a45d1bfea8a29f1d62e98385d58ece7d9314
kevincefang <angularfang@gmail.com>
2025-02-07 20:32:00 +0800

    add

--- lv0/task02/02数据类型/FunctionDemo.sol
+++ lv0/task02/02数据类型/FunctionDemo.sol
@@ -2,30 +2,41 @@
 pragma solidity >0.8.0;
 contract FunctionDemo {
 
+
     bytes4 public storedSelector ;
+
+    
 
     function square(uint x) public pure returns (uint){
         return x * x ;
     }
 
-    funcion double(uint x) public pure returns (uint){
+    function double(uint x) public pure returns (uint){
         return 2 * x;
     }
 
 
+
+    function getSquareSelector() public pure returns (bytes4){
+        return this.square.selector;
+    }
+
+
     function executeFunction(bytes4 selector,uint x) public returns (uint){
+    
         (bool success,bytes memory data) = address(this).call(abi.encodeWithSelector(selector,x));
-        require(success,"Function call failed.")
+        require(success,"Function call failed.");
         return abi.decode(data,(uint));
     }
 
-    function storedSelector(bytes _selector) public {
+    function storeSelector(bytes4 _selector) public {
          storedSelector = _selector;
     }
 
     function execcuteStoredSelector(uint x) public returns (uint){
-        require(storedSelector != bytes[4],"Selector not set");
-        (bool sucess,bytes memory data) = address(this).call(abi.encodeWithSelector(storedSelector,x));
+        require(storedSelector != 0,"Selector not set");
+        (bool success,bytes memory data) = address(this).call(abi.encodeWithSelector(storedSelector,x));
+        require(success,"Function call failed.");
         return abi.decode(data,(uint));
     }
 
